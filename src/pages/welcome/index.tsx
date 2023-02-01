217cb0345fd816790e203b5619294b24847d8ee6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 17:15:23 +0530

    adding welcome page component

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,7 +1,20 @@
+import HeadComp from '@components/common/headcomponent';
+import WelcomePageButton from '@components/welcome/buttons';
+import WelcomePageMainHeader from '@components/welcome/mainheader';
 import type { NextPage } from 'next';
 
 const WelcomePage: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <HeadComp headTitle='Welcome'></HeadComp>
+      <div className='h-screen w-screen'>
+        <div className='mx-auto w-fit pt-24'>
+          <WelcomePageMainHeader></WelcomePageMainHeader>
+          <WelcomePageButton></WelcomePageButton>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WelcomePage;
