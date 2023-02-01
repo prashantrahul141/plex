fb212f6d42616fc22096e189c48f51a2854cb50e
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 20:33:39 +0530

    redirect to signin page

--- src/components/welcome/buttons.tsx
+++ src/components/welcome/buttons.tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/router';
 import type { FC } from 'react';
 
 const WelcomePageButton: FC = () => {
+  const router = useRouter();
   return (
     <div className='mt-60 flex gap-4'>
-      <button className='btn rounded-lg bg-themePrimary-300 hover:border-themePrimary-300/80 hover:bg-themePrimary-300/80 hover:text-themePrimary-50'>
-        Get Started
+      <button
+        onClick={() => void router.push('/signin')}
+        className='btn rounded-lg bg-themePrimary-300 hover:border-themePrimary-300/80 hover:bg-themePrimary-300/80 hover:text-themePrimary-50'>
+        Sign in
       </button>
-      <button className='btn rounded-md text-themePrimary-50/80 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
+      <button className='btn rounded-md text-themePrimary-50/70 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
         Learn more
       </button>
     </div>
