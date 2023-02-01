1b1a22f6783ac46a3955a4354d768fa5af28ddc5
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 16:20:39 +0530

    adding home index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import HeadComp from '@components/common/headcomponent';
+import LoadingComponent from '@components/common/loadingcomponent';
 import { type NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
-  return <></>;
+  const { status } = useSession();
+  const router = useRouter();
+  if (status === 'authenticated') {
+    void router.push('/home');
+  } else if (status === 'unauthenticated') {
+    void router.push('/welcome');
+  }
+  return (
+    <>
+      <HeadComp headTitle='loading...'></HeadComp>
+
+      <div className='absolute left-1/2 top-1/2 h-6 w-6 translate-x-1/2'>
+        <LoadingComponent></LoadingComponent>
+      </div>
+    </>
+  );
 };
 
 export default Home;
