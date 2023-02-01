28e4aed772f66a41ebe58e6dc22a962f218b9824
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 20:34:17 +0530

    adding down button

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -8,9 +8,14 @@ const WelcomePage: NextPage = () => {
     <>
       <HeadComp headTitle='Welcome'></HeadComp>
       <div className='h-screen w-screen'>
-        <div className='mx-auto w-fit pt-24'>
+        <div className='mx-auto h-full w-fit pt-24'>
           <WelcomePageMainHeader></WelcomePageMainHeader>
           <WelcomePageButton></WelcomePageButton>
+          <div className='relative mx-auto mt-24 h-8 w-8 animate-bounce rounded-full bg-themePrimary-300 shadow-lg shadow-white/[0.15]'>
+            <span className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-themePrimary-50 '>
+              ↓
+            </span>
+          </div>
         </div>
       </div>
     </>
