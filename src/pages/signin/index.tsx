1417bad22fa41df22f6dd39f1595bbeb67e4ebd8
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 15:38:32 +0530

    adding a welcome and cancel text

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -13,14 +13,25 @@ const SignInPage: NextPage = () => {
     void router.push('/');
   } else if (status === 'unauthenticated') {
     return (
-      <div className='flex h-screen items-center justify-center'>
-        <SignInForm></SignInForm>
-      </div>
+      <>
+        <div className='flex h-screen flex-col items-center justify-center'>
+          <span className='mb-12 font-unbounded text-3xl tracking-wider text-themePrimary-50'>
+            Welcome
+          </span>
+          <SignInForm></SignInForm>
+          <div className='mt-8'>
+            <button className='btn border-none font-mukta text-sm text-themePrimary-100/60 hover:border-none hover:bg-transparent hover:text-themePrimary-200 hover:underline'>
+              cancel
+            </button>
+          </div>
+        </div>
+      </>
     );
   }
   return (
     <>
       <HeadComp headTitle='loading...'></HeadComp>
+
       <div className='absolute left-1/2 top-1/2 h-6 w-6 translate-x-1/2'>
         <LoadingComponent></LoadingComponent>
       </div>
