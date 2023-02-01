7ecd84afa28a8f7d64b0471b912f1ee5b4281a05
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 13:55:16 +0530

    adding compoent and redirects

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,7 +1,31 @@
+import HeadComp from '@components/common/headcomponent';
+import LoadingComponent from '@components/common/loadingcomponent';
+import SignInForm from '@components/forms/signinform';
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const SignInPage: NextPage = () => {
-  return <></>;
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === 'authenticated') {
+    void router.push('/');
+  } else if (status === 'unauthenticated') {
+    return (
+      <>
+        <SignInForm></SignInForm>
+      </>
+    );
+  }
+  return (
+    <>
+      <HeadComp headTitle='loading...'></HeadComp>
+      <div className='absolute left-1/2 top-1/2 h-6 w-6 translate-x-1/2'>
+        <LoadingComponent></LoadingComponent>
+      </div>
+    </>
+  );
 };
 
 export default SignInPage;
