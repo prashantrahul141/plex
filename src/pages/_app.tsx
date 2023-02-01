d4fed31a5fa54f9f347f43e65ea5e06d557280f5
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 15:54:12 +0530

    using framer motion and background gradient component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,25 @@ import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 
 import { api } from '../utils/api';
+import { motion } from 'framer-motion';
 
 import '../styles/globals.css';
+import BackgroundGradient from '@components/common/backgroundgradient';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.75, ease: 'easeInOut' }}>
+      <BackgroundGradient></BackgroundGradient>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </motion.div>
   );
 };
 
