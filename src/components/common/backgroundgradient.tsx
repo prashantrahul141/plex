1499777feba445e7714a14a9ff127c806091f2b9
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-01 15:53:57 +0530

    adding background gradient component

---
+++ src/components/common/backgroundgradient.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+const BackgroundGradient: FC = () => {
+  return (
+    <div className='background-gradient fixed top-0 left-0 -z-50 h-screen w-screen'></div>
+  );
+};
+
+export default BackgroundGradient;
