1db7ea813140d77b1756a2caba98f52e39cecf26
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-02 00:38:45 +0530

    using components

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,21 +1,22 @@
 import HeadComp from '@components/common/headcomponent';
 import WelcomePageButton from '@components/welcome/buttons';
+import WelcomePageCards from '@components/welcome/cards';
+import WelcomePageDownButton from '@components/welcome/downbutton';
 import WelcomePageMainHeader from '@components/welcome/mainheader';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const WelcomePage: NextPage = () => {
+  const router = useRouter();
   return (
     <>
       <HeadComp headTitle='Welcome'></HeadComp>
       <div className='h-screen w-screen'>
         <div className='mx-auto h-full w-fit pt-24'>
           <WelcomePageMainHeader></WelcomePageMainHeader>
-          <WelcomePageButton></WelcomePageButton>
-          <div className='relative mx-auto mt-24 h-8 w-8 animate-bounce rounded-full bg-themePrimary-300 shadow-lg shadow-white/[0.15]'>
-            <span className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-themePrimary-50 '>
-              ↓
-            </span>
-          </div>
+          <WelcomePageButton router={router}></WelcomePageButton>
+          <WelcomePageDownButton router={router}></WelcomePageDownButton>
+          <WelcomePageCards></WelcomePageCards>
         </div>
       </div>
     </>
