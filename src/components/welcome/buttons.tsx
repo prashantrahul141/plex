de92b548010a4377932d126974819bcfd41cdda7
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-02 18:29:52 +0530

    styling changes

--- src/components/welcome/buttons.tsx
+++ src/components/welcome/buttons.tsx
@@ -6,12 +6,12 @@ const WelcomePageButton: FC<{ router: NextRouter }> = ({ router }) => {
     <div className='mx-auto mt-60 flex w-fit gap-4'>
       <button
         onClick={() => void router.push('/signin')}
-        className='btn hover:bg-themePrimary-30080 w-max rounded-lg bg-themePrimary-300 hover:bg-gradient-to-bl hover:from-themePrimary-50/20 hover:to-transparent hover:text-themePrimary-50 '>
+        className='btn hover:bg-themePrimary-30080 w-28 rounded-lg bg-themePrimary-300 hover:bg-gradient-to-bl hover:from-themePrimary-50/20 hover:to-transparent hover:text-themePrimary-50 '>
         Sign in
       </button>
       <button
         onClick={() => void router.push('/home')}
-        className='btn w-max rounded-md text-themePrimary-50/70 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
+        className='btn w-28 rounded-md text-themePrimary-50/70 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
         Explore
       </button>
     </div>
