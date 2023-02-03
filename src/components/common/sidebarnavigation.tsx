3996e2e4e72b917f806d863f876bcedc3d72a9dd
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-03 20:25:30 +0530

    moving NavMenuTabs

--- src/components/common/sidebarnavigation.tsx
+++ src/components/common/sidebarnavigation.tsx
@@ -1,114 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import type { FC } from 'react';
-import {
-  AiOutlineHome,
-  AiFillHome,
-  AiOutlineBell,
-  AiFillBell,
-  AiOutlineSetting,
-  AiTwotoneSetting,
-} from 'react-icons/ai';
-import {
-  HiOutlinePencil,
-  HiPencil,
-  HiOutlineGlobe,
-  HiGlobe,
-  HiOutlineBookmark,
-  HiBookmark,
-} from 'react-icons/hi';
+import { NavMenuTabs } from 'src/constants';
 
 const SideBarNavigation: FC<{
   activateTab: string;
   authorAvatar: string;
   authorName: string;
 }> = ({ activateTab, authorAvatar, authorName }) => {
-  const iconSize = 32;
-
-  const Tabs: Array<{
-    name: string;
-    link: string;
-    inactivateIcon: JSX.Element;
-    activateIcon: JSX.Element;
-  }> = [
-    {
-      name: 'Home',
-      link: '/home',
-      inactivateIcon: (
-        <AiOutlineHome
-          className='navbar-icon-inactive'
-          size={iconSize}></AiOutlineHome>
-      ),
-      activateIcon: (
-        <AiFillHome className='navbar-icon-active' size={iconSize}></AiFillHome>
-      ),
-    },
-    {
-      name: 'Create',
-      link: '/create',
-      inactivateIcon: (
-        <HiOutlinePencil
-          className='navbar-icon-inactive'
-          size={iconSize}></HiOutlinePencil>
-      ),
-      activateIcon: (
-        <HiPencil className='navbar-icon-active' size={iconSize}></HiPencil>
-      ),
-    },
-
-    {
-      name: 'Trending',
-      link: '/trending',
-      inactivateIcon: (
-        <HiOutlineGlobe
-          className='navbar-icon-inactive'
-          size={iconSize}></HiOutlineGlobe>
-      ),
-      activateIcon: (
-        <HiGlobe className='navbar-icon-active' size={iconSize}></HiGlobe>
-      ),
-    },
-
-    {
-      name: 'Notifications',
-      link: '/notifications',
-      inactivateIcon: (
-        <AiOutlineBell
-          className='navbar-icon-inactive'
-          size={iconSize}></AiOutlineBell>
-      ),
-      activateIcon: (
-        <AiFillBell className='navbar-icon-active' size={iconSize}></AiFillBell>
-      ),
-    },
-    {
-      name: 'Bookmarks',
-      link: '/bookmarks',
-      inactivateIcon: (
-        <HiOutlineBookmark
-          className='navbar-icon-inactive'
-          size={iconSize}></HiOutlineBookmark>
-      ),
-      activateIcon: (
-        <HiBookmark className='navbar-icon-active' size={iconSize}></HiBookmark>
-      ),
-    },
-    {
-      name: 'Settings',
-      link: '/settings',
-      inactivateIcon: (
-        <AiOutlineSetting
-          className='navbar-icon-inactive'
-          size={iconSize}></AiOutlineSetting>
-      ),
-      activateIcon: (
-        <AiTwotoneSetting
-          className='navbar-icon-active'
-          size={iconSize}></AiTwotoneSetting>
-      ),
-    },
-  ];
-
   return (
     <nav className='flex h-screen w-max flex-col'>
       <div className='mb-8 mt-4'>
@@ -116,7 +15,7 @@ const SideBarNavigation: FC<{
           <Image src='/favicon.ico' width={32} height={32} alt='Plex'></Image>
         </Link>
       </div>
-      {Tabs.map((eachTab, index) => {
+      {NavMenuTabs.map((eachTab, index) => {
         return (
           <div key={index} className='my-3 flex'>
             <Link href={eachTab.link}>
