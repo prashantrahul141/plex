e5a608f4e92a62d8e682be87e5059b02a6eb63f3
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-03 23:48:39 +0530

    basic form componenet

--- src/components/forms/createpostform.tsx
+++ src/components/forms/createpostform.tsx
@@ -1,7 +1,38 @@
 import type { FC } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+interface IFormInput {
+  postText: string;
+  postImages?: Array<string>;
+}
 
 const CreatePostForm: FC = () => {
-  return <></>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
+
+  const submitForm: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form className='w-full' onSubmit={handleSubmit(submitForm)}>
+      <input
+        className='w-full'
+        {...register('postText', { required: true, maxLength: 20 })}
+      />
+      {errors.postText?.type === 'required' && (
+        <p role='alert'>forgot to type something here?</p>
+      )}
+      <input type='file'></input>
+      <button className='btn' type='submit'>
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default CreatePostForm;
