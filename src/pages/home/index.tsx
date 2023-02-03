e275ba16766394eb90ba2cd26513c9580ba69cf6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-03 17:42:38 +0530

    placeholder data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,14 @@
+import SideBarNavigation from '@components/common/sidebarnavigation';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <SideBarNavigation
+        activateTab='Bookmarks'
+        authorName='Prashant'
+        authorAvatar='https://cdn.discordapp.com/avatars/995581232459038760/d37933badeb94c9f07d3fabc103df2f0.png'></SideBarNavigation>
+    </>
+  );
 };
 export default HomePage;
