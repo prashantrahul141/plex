fa278ac93db6f5ed20e89b138390cf7220374652
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-05 05:30:38 +0530

    adding alert component

---
+++ src/components/common/commonAlert.tsx
@@ -0,0 +1,26 @@
+import type { FC } from 'react';
+import { motion } from 'framer-motion';
+
+const CommonAlert: FC<{
+  alertText: string;
+  alertType: 'error' | 'warn' | 'success';
+}> = ({ alertText, alertType }) => {
+  return (
+    <motion.div
+      initial={{ bottom: -24 }}
+      exit={{ bottom: -24 }}
+      animate={{ bottom: 24 }}
+      transition={{ duration: 0.5, type: 'spring' }}
+      className={`fixed bottom-6 left-1/2 flex -translate-x-1/2 items-center justify-center rounded-md px-2 py-1 ${
+        alertType === 'success' ? 'bg-green-400' : ''
+      } ${alertType === 'warn' ? 'bg-yellow-400' : ''} ${
+        alertType === 'error' ? 'bg-red-400' : ''
+      }`}>
+      <span className='select-none font-mukta leading-snug text-black'>
+        {alertText}
+      </span>
+    </motion.div>
+  );
+};
+
+export default CommonAlert;
