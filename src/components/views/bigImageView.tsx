6680d45a464bc3ea0d23e942adf89e2c7d6ad956
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-05 21:38:48 +0530

    adding width property for bigImage component

--- src/components/views/bigImageView.tsx
+++ src/components/views/bigImageView.tsx
@@ -5,14 +5,15 @@ import { motion } from 'framer-motion';
 const BigImageView: FC<{
   imageUrl: string;
   callBackFun: (_state: boolean) => void;
-}> = ({ imageUrl, callBackFun }) => {
+  width?: number;
+}> = ({ imageUrl, callBackFun, width }) => {
   return (
     <>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.2 }}
         onClick={() => callBackFun(false)}
         className='fixed top-0 left-0 h-screen w-screen backdrop-blur-sm backdrop-brightness-50'>
         <motion.div
@@ -21,12 +22,22 @@ const BigImageView: FC<{
           animate={{ scale: 1, opacity: 1 }}
           exit={{ scale: 0, opacity: 1 }}
           transition={{ duration: 0.5, type: 'spring' }}>
-          <Image
-            className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'
-            src={imageUrl}
-            width={1000}
-            height={1000}
-            alt='Image'></Image>
+          {width === undefined && (
+            <Image
+              className='absolute top-1/2 left-1/2 w-full object-contain'
+              src={imageUrl}
+              fill
+              alt='Image'></Image>
+          )}
+
+          {width !== undefined && (
+            <Image
+              className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+              src={imageUrl}
+              width={width}
+              height={width}
+              alt='Image'></Image>
+          )}
         </motion.div>
       </motion.div>
     </>
