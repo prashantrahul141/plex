1d467ed023eabddbdfd342e46835bb8be63af5f6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-06 20:23:19 +0530

    removing unessary comment

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,8 @@
 import { createTRPCRouter } from './trpc';
 import { PingRouter } from './routers/ping';
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here
- */
 export const appRouter = createTRPCRouter({
   ping: PingRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
