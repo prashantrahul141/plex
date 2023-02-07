9e785f54dd761f4bfe34be2aadd999fd0c552af9
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-07 19:10:59 +0530

    styling for top and side navigation bar

--- src/components/common/pagelayout.tsx
+++ src/components/common/pagelayout.tsx
@@ -1,56 +1,71 @@
 import type { FC } from 'react';
-import HeadComp from './headcomponent';
 import { AnimatePresence, motion } from 'framer-motion';
 import NotificationsList from '@components/lists/notificationsList';
 import PostList from '@components/lists/postsLists';
 import BookmarksList from '@components/lists/bookmarksList';
-import CreatePostForm from '@components/forms/createpostform';
 import TrendingList from '@components/lists/trendingList';
 import SettingsForm from '@components/forms/settingsform';
 import TopBarNavigation from './topbarnavigation';
 import { useSession } from 'next-auth/react';
-import LoadingComponent from './loadingcomponent';
+import SideBarNavigation from './sidebarnavigation';
+import { useRouter } from 'next/router';
 
 type pages =
   | 'home'
-  | 'create'
   | 'trending'
   | 'notifications'
   | 'bookmarks'
   | 'settings'
   | 'profile';
 
-const PageLayout: FC<{ title?: string; page: pages }> = ({ title, page }) => {
+const PageLayout: FC<{ page: pages }> = ({ page }) => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   if (session) {
     return (
-      <div>
-        <HeadComp headTitle={title}></HeadComp>
-        <TopBarNavigation
-          activeTab={page}
-          authorAvatar={
-            session.user.image || '/public/static/defaultAvatar.png'
-          }
-          authorName={session.user.name || 'user'}></TopBarNavigation>
-        <AnimatePresence>
-          <div className='relative'>
-            <motion.div className='absolute'>
-              {page === 'home' && <PostList></PostList>}
-              {page === 'create' && <CreatePostForm></CreatePostForm>}
-              {page === 'trending' && <TrendingList></TrendingList>}
-              {page === 'notifications' && (
-                <NotificationsList></NotificationsList>
-              )}
-              {page === 'bookmarks' && <BookmarksList></BookmarksList>}
-              {page === 'settings' && <SettingsForm></SettingsForm>}
-            </motion.div>
+      <>
+        <div className='flex w-full flex-col gap-2 sm:flex-row sm:gap-0'>
+          <div className='z-50 w-full sm:hidden'>
+            <TopBarNavigation
+              activeTab={page}
+              authorAvatar={
+                session.user.image || '/public/static/defaultAvatar.png'
+              }
+              authorName={session.user.name || 'user'}></TopBarNavigation>
           </div>
-        </AnimatePresence>
-      </div>
+
+          <div className='hidden sm:block'>
+            <SideBarNavigation
+              activeTab={page}
+              authorAvatar={
+                session.user.image || '/public/static/defaultAvatar.png'
+              }
+              authorName={session.user.name || 'user'}></SideBarNavigation>
+          </div>
+
+          <AnimatePresence>
+            <div className='max-w-2xl flex-grow'>
+              <motion.div className=''>
+                {page === 'home' && <PostList></PostList>}
+
+                {page === 'trending' && <TrendingList></TrendingList>}
+                {page === 'notifications' && (
+                  <NotificationsList></NotificationsList>
+                )}
+                {page === 'bookmarks' && <BookmarksList></BookmarksList>}
+                {page === 'settings' && <SettingsForm></SettingsForm>}
+              </motion.div>
+            </div>
+          </AnimatePresence>
+        </div>
+        <div></div>
+      </>
     );
+  } else {
+    void router.push('/');
   }
-  return <LoadingComponent></LoadingComponent>;
+  return <></>;
 };
 
 export default PageLayout;
