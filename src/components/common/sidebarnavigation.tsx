99572c9d0a0eb9082a526d09b5252bdf4ab7c4d3
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-07 18:37:37 +0530

    adding create form

--- src/components/common/sidebarnavigation.tsx
+++ src/components/common/sidebarnavigation.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import type { FC } from 'react';
+import { useState } from 'react';
 import { getNavMenuTabs } from 'src/constants';
+import { AnimatePresence, motion } from 'framer-motion';
+import { HiOutlinePencil, HiPencil } from 'react-icons/hi';
+import CreatePostForm from '@components/forms/createpostform';
 
 const SideBarNavigation: FC<{
   activeTab: string;
   authorAvatar: string;
   authorName: string;
 }> = ({ activeTab, authorAvatar, authorName }) => {
+  const [showCreatePostForm, setShowCreatePostForm] = useState(false);
+
   return (
     <nav className='flex h-screen w-max flex-col'>
       <div className='mb-8 mt-4'>
@@ -15,6 +21,23 @@ const SideBarNavigation: FC<{
           <Image src='/favicon.ico' width={32} height={32} alt='Plex'></Image>
         </Link>
       </div>
+
+      <div className='my-3 flex' onClick={() => setShowCreatePostForm(true)}>
+        <i className='group relative flex items-center'>
+          {!showCreatePostForm && (
+            <HiOutlinePencil
+              size={32}
+              className='navbar-icon-inactive'></HiOutlinePencil>
+          )}
+          {showCreatePostForm && (
+            <HiPencil size={32} className='navbar-icon-active'></HiPencil>
+          )}
+          <span className='absolute left-full ml-1 hidden rounded-md bg-themePrimary-50/70 px-1  font-mukta text-sm not-italic leading-relaxed text-black group-hover:block'>
+            Create
+          </span>
+        </i>
+      </div>
+
       {getNavMenuTabs().map((eachTab, index) => {
         return (
           <div key={index} className='my-3 flex'>
@@ -31,6 +54,7 @@ const SideBarNavigation: FC<{
           </div>
         );
       })}
+
       <div className='my-8 flex'>
         <Link href={'/profile'}>
           <i className='group relative flex items-center'>
@@ -46,6 +70,28 @@ const SideBarNavigation: FC<{
           </i>
         </Link>
       </div>
+
+      <AnimatePresence>
+        {showCreatePostForm && (
+          <>
+            <div
+              onClick={() => setShowCreatePostForm(false)}
+              className='fixed top-0 left-0 z-40 h-screen w-screen cursor-default backdrop-brightness-75'></div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+                translateX: '-50%',
+              }}
+              exit={{ opacity: 0, scale: 0, translateX: '-75%' }}
+              transition={{ type: 'spring', duration: 0.4 }}
+              className='fixed top-40 left-1/2 z-50 flex w-full -translate-x-1/2 cursor-default items-center justify-center'>
+              <CreatePostForm></CreatePostForm>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
