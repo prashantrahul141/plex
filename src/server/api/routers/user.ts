6f3eb9cae1ad009c50d84f96beee1e0f7ffb27cf
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-13 02:10:25 +0530

    adding get user api endpoint

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,39 @@
+import { prisma } from 'src/server/db';
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '../trpc';
+
+export const UserRouter = createTRPCRouter({
+  get: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const foundUser = await prisma.user.findUnique({
+        where: {
+          username: input.username,
+        },
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          image: true,
+          banner: true,
+          url: true,
+          bio: true,
+          joinedOn: true,
+          authorVerified: true,
+          _count: { select: { followers: true, followings: true } },
+        },
+      });
+      if (foundUser) {
+        foundUser.id;
+      }
+
+      return {
+        foundUser,
+        isAuthor: foundUser ? foundUser.id === ctx.session?.user.id : false,
+      };
+    }),
+});
