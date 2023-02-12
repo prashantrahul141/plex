844793bc4401140ebb6f0de336ed06de58f84d64
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-12 17:38:27 +0530

    adding post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from './trpc';
 import { PingRouter } from './routers/ping';
+import { PostRouter } from './routers/post';
 
 export const appRouter = createTRPCRouter({
   ping: PingRouter,
+  post: PostRouter,
 });
 
 export type AppRouter = typeof appRouter;
