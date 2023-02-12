d45bdadb7546cd11c866f4d8c312bd4d1c5b27f0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-13 02:13:36 +0530

    adding user router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from './trpc';
 import { PingRouter } from './routers/ping';
 import { PostRouter } from './routers/post';
+import { UserRouter } from './routers/user';
 
 export const appRouter = createTRPCRouter({
   ping: PingRouter,
   post: PostRouter,
+  user: UserRouter,
 });
 
 export type AppRouter = typeof appRouter;
