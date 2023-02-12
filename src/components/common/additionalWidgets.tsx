09401cdf4c168abda089eb64973702fd00d512ae
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-12 19:31:48 +0530

    right side bar

---
+++ src/components/common/additionalWidgets.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+const AdditionalWidgets: FC = () => {
+  return (
+    <aside className='h-screen w-52 border-l border-themePrimary-100/40'></aside>
+  );
+};
+
+export default AdditionalWidgets;
