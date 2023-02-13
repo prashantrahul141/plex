726921ff7b9b04c4874d3b21a5264e096291a690
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-13 17:24:33 +0530

    adding getFromId

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,6 +3,40 @@ import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '../trpc';
 
 export const UserRouter = createTRPCRouter({
+  getFromId: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const foundUser = await prisma.user.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          image: true,
+          banner: true,
+          url: true,
+          bio: true,
+          joinedOn: true,
+          authorVerified: true,
+          _count: { select: { followers: true, followings: true } },
+        },
+      });
+      if (foundUser) {
+        foundUser.id;
+      }
+
+      return {
+        foundUser,
+        isAuthor: foundUser ? foundUser.id === ctx.session?.user.id : false,
+      };
+    }),
+
   get: publicProcedure
     .input(
       z.object({
