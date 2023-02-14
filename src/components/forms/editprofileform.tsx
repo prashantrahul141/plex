f2b1690cadb83173327da57e2a3e64820201a27d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 00:29:47 +0530

    basic component

--- src/components/forms/editprofileform.tsx
+++ src/components/forms/editprofileform.tsx
@@ -1,8 +1,27 @@
 import type { FC } from 'react';
-import type { Session } from 'next-auth';
 import { api } from '@utils/api';
+import { useRouter } from 'next/router';
+import LoadingComponent from '@components/common/loadingcomponent';
 
-const EditProfileForm: FC<{ session: Session }> = ({ session }) => {
+const EditProfileForm: FC = () => {
+  const UserData = api.user.getForEdit.useQuery();
+  const router = useRouter();
+
+  if (UserData.status !== 'success') {
+    return (
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='h-8 w-8'>
+          <LoadingComponent></LoadingComponent>
+        </div>
+      </div>
+    );
+  }
+
+  if (UserData.data !== undefined && UserData.data.UserData === null) {
+    void router.push('/404');
+    return <></>;
+  }
+
   return <></>;
 };
 
