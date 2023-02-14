58e64a131522ffa5f09fced460b9028a95d4358d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-14 20:18:46 +0530

    fixing import eslint

--- src/components/common/pagelayout.tsx
+++ src/components/common/pagelayout.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from 'react';
+import type { FC } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import NotificationsList from '@components/lists/notificationsList';
 import PostList from '@components/lists/postsLists';
