f440d405e459da17e332997b7e006a21442b31ff
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 01:00:52 +0530

    adding file

---
+++ src/components/forms/editprofileimages.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+const EditProfileImagesForm: FC<{
+  currentAvatar: string;
+  currentBanner: string;
+}> = () => {
+  return <></>;
+};
+export default EditProfileImagesForm;
---
+++ src/components/forms/editprofileusernameform.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+const EditProfileUsernameForm: FC<{ currentUsername: string }> = ({
+  currentUsername,
+}) => {
+  return <></>;
+};
+
+export default EditProfileUsernameForm;
