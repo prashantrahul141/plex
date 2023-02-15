69313cde1c63bde035f9b9fcf2584e622119fefc
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 22:50:49 +0530

    adding username validation

--- src/components/forms/editprofileusernameform.tsx
+++ src/components/forms/editprofileusernameform.tsx
@@ -1,17 +1,44 @@
 import type { FC } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
-import type { UseFormRegister } from 'react-hook-form';
+import type { UseFormRegister, UseFormWatch } from 'react-hook-form';
 import type { IEditFormInput } from 'src/types';
-import { BiErrorCircle, BiCheckCircle } from 'react-icons/bi';
+import { BiErrorCircle, BiLoaderAlt, BiCheckCircle } from 'react-icons/bi';
+import { api } from '@utils/api';
 
 const EditProfileUsernameForm: FC<{
+  watch: UseFormWatch<IEditFormInput>;
   currentUsername: string;
   register: UseFormRegister<IEditFormInput>;
-}> = ({ currentUsername, register }) => {
-  const checkIfAlreadyExists = (value: string) => {
-    return !['abc', 'sans', 'sds'].includes(value);
-  };
+}> = ({ currentUsername, register, watch }) => {
   const [existsAlready, setExistsAlready] = useState(false);
+  const [loadingUsernameCheck, setLoadingUsernameCheck] = useState(false);
+  const usernameState = watch('username', currentUsername);
+  const usernameCheckQuery = api.user.checkUsername.useQuery(
+    { username: usernameState },
+    { enabled: false }
+  );
+
+  useEffect(() => {
+    if (usernameState === currentUsername) {
+      setExistsAlready(false);
+      return;
+    }
+    setLoadingUsernameCheck(true);
+    const fetchData = async () => {
+      const result = await usernameCheckQuery.refetch();
+      return result.data || false;
+    };
+    fetchData()
+      .then((data) => {
+        setExistsAlready(data);
+        setLoadingUsernameCheck(false);
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [usernameState]);
 
   return (
     <div className='relative flex items-center'>
@@ -23,17 +50,22 @@ const EditProfileUsernameForm: FC<{
         defaultValue={currentUsername}
         {...register('username', {
           required: { value: true, message: 'Forgot to type your username?' },
-          validate: checkIfAlreadyExists,
           minLength: {
             value: 3,
             message: 'username cannot be shorter than 3 letters',
           },
+          validate: () => {
+            return existsAlready;
+          },
         })}
       />
-      {existsAlready && (
+      {loadingUsernameCheck && (
+        <BiLoaderAlt className='absolute right-2 text-xl text-blue-400'></BiLoaderAlt>
+      )}
+      {existsAlready && !loadingUsernameCheck && (
         <BiErrorCircle className='absolute right-2 text-xl text-red-400'></BiErrorCircle>
       )}
-      {!existsAlready && (
+      {!existsAlready && !loadingUsernameCheck && (
         <BiCheckCircle className='absolute right-2 text-xl text-green-400'></BiCheckCircle>
       )}
     </div>
