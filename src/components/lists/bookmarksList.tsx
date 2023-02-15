22179b8372781f8f19ea0a79dccada6da1c15050
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 14:26:15 +0530

    adding component

--- src/components/lists/bookmarksList.tsx
+++ src/components/lists/bookmarksList.tsx
@@ -1,7 +1,32 @@
 import type { FC } from 'react';
+import { api } from '@utils/api';
+import LoadingComponent from '@components/common/loadingcomponent';
+import PostView from '@components/views/postView';
 
-const BookmarksList: FC = () => {
-  return <></>;
+const BookmarksList: FC<{ authorId: string }> = ({ authorId }) => {
+  const bookmarkedPosts = api.post.getBookmarks.useQuery();
+  if (bookmarkedPosts.status !== 'success') {
+    return (
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='h-8 w-8'>
+          <LoadingComponent></LoadingComponent>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className=''>
+      {bookmarkedPosts.data.bookmarkedPosts.map((eachPost) => {
+        return (
+          <PostView
+            key={eachPost.post.id}
+            data={{ post: eachPost.post }}
+            currentUserID={authorId}></PostView>
+        );
+      })}
+    </div>
+  );
 };
 
 export default BookmarksList;
