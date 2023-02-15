950b49d5a6ba203e13a56ce05812c175890af33d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 19:07:12 +0530

    separate component for form errors

---
+++ src/components/common/errorMessage.tsx
@@ -0,0 +1,17 @@
+import type { FC } from 'react';
+import { BiErrorCircle } from 'react-icons/bi';
+
+const ErrorMessage: FC<{ message: string | undefined }> = ({ message }) => {
+  return (
+    <div title={message}>
+      <span
+        role='alert'
+        className='ml-1 flex w-fit items-center gap-1 rounded-md border border-red-500/40 bg-red-100/90 px-1 font-mukta text-sm text-red-700'>
+        <BiErrorCircle></BiErrorCircle>
+        {message}
+      </span>
+    </div>
+  );
+};
+
+export default ErrorMessage;
