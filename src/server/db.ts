35831666b943f41ec485f18fe2ad506cde01732d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 17:28:30 +0530

    preetier

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-import { env } from "../env/server.mjs";
+import { env } from '../env/server.mjs';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
@@ -8,7 +8,7 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
