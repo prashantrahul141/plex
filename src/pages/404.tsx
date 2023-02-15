f7d7e96cebf66e370512c23ebb21ff4012551ad0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-15 17:31:07 +0530

    removing query to log in prisma

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,15 @@
 import type { FC } from 'react';
 
+import { signOut } from 'next-auth/react';
 const FourZeroFour: FC = () => {
-  return <></>;
+  return (
+    <>
+      <button
+        className='btn'
+        onClick={async () => {
+          await signOut();
+        }}></button>
+    </>
+  );
 };
 export default FourZeroFour;
