31fc1de40190a077997c8dd0c57f2f85e2bfbf91
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-18 01:45:04 +0530

    adding notification router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,11 +2,13 @@ import { createTRPCRouter } from './trpc';
 import { PingRouter } from './routers/ping';
 import { PostRouter } from './routers/post';
 import { UserRouter } from './routers/user';
+import { NotificationRouter } from './routers/notification';
 
 export const appRouter = createTRPCRouter({
   ping: PingRouter,
   post: PostRouter,
   user: UserRouter,
+  notification: NotificationRouter,
 });
 
 export type AppRouter = typeof appRouter;
