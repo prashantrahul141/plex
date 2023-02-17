ec6915a5266f3a51f76f8201307b882eccaf1915
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-17 20:49:52 +0530

    adding keys to main content

--- src/components/layouts/pagelayout.tsx
+++ src/components/layouts/pagelayout.tsx
@@ -52,36 +52,46 @@ const PageLayout: FC<{ page: pages }> = ({ page }) => {
         </div>
 
         <AnimatePresence>
-          <div className='max-w-lg overflow-auto sm:min-w-[40rem] md:border-r md:border-themePrimary-100/40'>
-            <h2 className='hidden border-b border-themePrimary-100/40 px-3 py-4 font-mukta text-2xl capitalize tracking-wide text-themePrimary-50/90 sm:block'>
+          <div
+            key={0}
+            className='max-w-lg overflow-auto sm:min-w-[40rem] md:border-r md:border-themePrimary-100/40'>
+            <h2
+              key={1}
+              className='hidden border-b border-themePrimary-100/40 px-3 py-4 font-mukta text-2xl capitalize tracking-wide text-themePrimary-50/90 sm:block'>
               {['user', 'profile'].includes(page) ? layoutTitle : page}
             </h2>
-            <motion.div className='h-[90%] sm:overflow-auto'>
+            <motion.div key={2} className='h-[90%] sm:overflow-auto'>
               {page === 'home' && (
                 <PostList authorId={session.user.id}></PostList>
               )}
 
-              {page === 'trending' && <TrendingList></TrendingList>}
+              {page === 'trending' && <TrendingList key={3}></TrendingList>}
               {page === 'notifications' && (
-                <NotificationsList></NotificationsList>
+                <NotificationsList key={4}></NotificationsList>
               )}
               {page === 'bookmarks' && (
-                <BookmarksList authorId={session.user.id}></BookmarksList>
+                <BookmarksList
+                  key={5}
+                  authorId={session.user.id}></BookmarksList>
               )}
-              {page === 'settings' && <SettingsForm></SettingsForm>}
+              {page === 'settings' && <SettingsForm key={6}></SettingsForm>}
 
               {page === 'profile' && (
                 <UserProfileView
+                  key={7}
                   setLayoutTitleCallback={(target: string) =>
                     setLayoutTitle(target)
                   }
                   isCurrentUser={true}
                   session={session}></UserProfileView>
               )}
-              {page === 'edit profile' && <EditProfileForm></EditProfileForm>}
+              {page === 'edit profile' && (
+                <EditProfileForm key={8}></EditProfileForm>
+              )}
 
               {page === 'user' && (
                 <UserProfileView
+                  key={9}
                   setLayoutTitleCallback={(target: string) =>
                     setLayoutTitle(target)
                   }
