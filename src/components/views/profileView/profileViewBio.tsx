113c2b6f0fd366e8cbe37c8b14900db399f627b4
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-17 21:48:13 +0530

    parsing bio

--- src/components/views/profileView/profileViewBio.tsx
+++ src/components/views/profileView/profileViewBio.tsx
@@ -1,13 +1,55 @@
 import type { FC } from 'react';
+import Link from 'next/link';
 
 const ProfileViewBio: FC<{ bioText: string | null }> = ({ bioText }) => {
   const parseBio = (text: string) => {
-    return text;
+    const bioArray = text.split(' ');
+    const resultedBio: Array<JSX.Element> = [];
+
+    for (let i = 0; i < bioArray.length; i++) {
+      const element = bioArray[i];
+      if (element) {
+        try {
+          const bioUrl = new URL(element);
+          resultedBio.push(
+            <Link
+              className='font-ibmplex leading-tight tracking-tighter text-themePrimary-300 hover:underline'
+              key={i}
+              title={bioUrl.origin}
+              href={bioUrl.href}>
+              {`${bioUrl.hostname} `}
+            </Link>
+          );
+          continue;
+        } catch {}
+
+        const startLetter = element[0];
+        if (startLetter) {
+          if (startLetter === '@') {
+            resultedBio.push(
+              <Link
+                className='font-ibmplex leading-tight tracking-tighter text-themePrimary-300 hover:underline'
+                key={i}
+                title={element}
+                href={`/${element.substring(1, element.length)}`}>
+                {`${element} `}
+              </Link>
+            );
+            continue;
+          }
+        }
+
+        resultedBio.push(<span key={i}>{`${element} `}</span>);
+      }
+    }
+    console.log(resultedBio);
+
+    return resultedBio;
   };
 
   if (bioText) {
     return (
-      <span className='mb-1 block whitespace-pre-line font-mukta font-thin leading-snug text-themePrimary-50/95'>
+      <span className='mb-1 block whitespace-pre-line font-mukta font-thin leading-snug tracking-wide text-themePrimary-50/95'>
         {parseBio(bioText)}
       </span>
     );
