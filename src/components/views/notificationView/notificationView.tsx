13db3f1bf56072c236830ff89a3223107a613fc6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-18 15:28:13 +0530

    adding component

--- src/components/views/notificationView/notificationView.tsx
+++ src/components/views/notificationView/notificationView.tsx
@@ -1,19 +1,34 @@
 import type { FC } from 'react';
+import type { Notification } from '@prisma/client';
+import ReactTimeAgo from 'react-time-ago';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const NotificationView: FC<{
-  text: string;
-  icon: string;
-  url: string;
-  createdOn: string;
-  seen: boolean;
-}> = ({ text, icon }) => {
+const NotificationView: FC<{ data: Notification }> = ({ data }) => {
   return (
-    <article className='flex w-full items-center justify-center gap-2'>
-      <div className='text-2xl text-themePrimary-50/95'></div>
-      <div className='flex-grow font-mukta leading-none tracking-wide text-themePrimary-50/95'>
-        <p>{text}</p>
+    <Link
+      href={data.url}
+      className='border-y-1 flex w-full items-center justify-center gap-2 border-b border-themePrimary-50/30 px-1 py-2 hover:bg-themePrimary-50/5'>
+      <div className='h-10 w-10 text-2xl text-themePrimary-50/95'>
+        <Image
+          alt='Icon'
+          src={data.iconImage}
+          className='object-cover'
+          width={100}
+          height={100}></Image>
       </div>
-    </article>
+
+      <div className='flex-grow font-mukta leading-none tracking-wide text-themePrimary-50/95'>
+        <p>{data.text}</p>
+      </div>
+
+      <div>
+        <ReactTimeAgo
+          className='font-ibmplex text-xs text-themePrimary-50/70'
+          date={data.createdOn}
+          timeStyle={'twitter'}></ReactTimeAgo>
+      </div>
+    </Link>
   );
 };
 export default NotificationView;
