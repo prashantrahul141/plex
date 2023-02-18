5b2ad1388e96cf1cfeb17f518bd18d710b7ecb92
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-18 15:28:20 +0530

    adding component

--- src/components/lists/notificationsList.tsx
+++ src/components/lists/notificationsList.tsx
@@ -1,7 +1,31 @@
+import { api } from '@utils/api';
 import type { FC } from 'react';
+import LoadingComponent from '@components/common/loadingcomponent';
+import NotificationView from '@components/views/notificationView/notificationView';
 
 const NotificationsList: FC = () => {
-  return <></>;
+  const notifications = api.notification.get.useQuery();
+
+  if (notifications.status !== 'success') {
+    return (
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='h-8 w-8'>
+          <LoadingComponent></LoadingComponent>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className=''>
+      {notifications.data.map((eachNotification) => {
+        return (
+          <NotificationView
+            key={eachNotification.id}
+            data={eachNotification}></NotificationView>
+        );
+      })}
+    </div>
+  );
 };
 
 export default NotificationsList;
