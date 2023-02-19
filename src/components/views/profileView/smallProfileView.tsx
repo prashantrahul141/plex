44489423d77b0dc5c669bb76180def1463765418
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-19 21:31:23 +0530

    styling and using proper props

--- src/components/views/profileView/smallProfileView.tsx
+++ src/components/views/profileView/smallProfileView.tsx
@@ -1,63 +1,50 @@
 import type { FC } from 'react';
 import Image from 'next/image';
-import { useState } from 'react';
 import { MdVerified } from 'react-icons/md';
 import type { IReturnSmallUser } from 'src/types';
-
+import Link from 'next/link';
+import ProfileViewBio from './profileViewBio';
 const SmallProfileView: FC<{
   data: IReturnSmallUser;
 }> = ({ data }) => {
-  const [following, setFollowing] = useState(isFollowed);
-
-  const handleFollow = () => {
-    setFollowing(!following);
-    // TODO: Handle follow
-  };
-
   return (
-    <article className='flex'>
-      <div className=''>
-        <Image
-          className='w-12 rounded-full'
-          width={50}
-          height={50}
-          alt={userUsername}
-          src={userAvatar}></Image>
-      </div>
-      <main className='relative flex-grow pt-1 pl-2'>
-        <header className=''>
-          <span className='flex gap-1 font-mukta text-xl leading-none tracking-wider text-themePrimary-50'>
-            {userName}
-            {userVerified && (
-              <span className='group/verified relative'>
-                <MdVerified></MdVerified>
-                <span className='absolute left-1/2 top-6 hidden w-max -translate-x-1/2 rounded-md border border-none bg-black/90 px-2 py-1 font-mukta text-xs font-thin tracking-wide text-themePrimary-50/90 group-hover/verified:block'>
-                  This user is verified by the plex team.
-                </span>
-              </span>
-            )}
-          </span>
-          <span className='font-ibmplex text-sm leading-none tracking-wide text-themePrimary-50/60'>
-            @{userUsername}
-          </span>
-        </header>
-        {!isAuthor && (
-          <span className='absolute top-2 right-2 '>
-            <button
-              onClick={handleFollow}
-              className={`btn w-fit rounded-3xl px-3 font-mukta text-sm ${
-                !following ? 'bg-themePrimary-300 text-themePrimary-50/90' : ''
-              }`}>
-              {following ? 'Following' : 'Follow'}
-            </button>
-          </span>
-        )}
-        <div className='mt-3'>
-          <p className='font-mukta font-thin leading-tight tracking-wide text-themePrimary-50/95'>
-            {userBio}
-          </p>
+    <article className='border-y border-y-themePrimary-100/40'>
+      <Link className='flex py-2 px-1' href={'/' + data.username}>
+        <div className=''>
+          <Image
+            className='h-12 min-h-[3rem] w-12 min-w-[3rem] rounded-full object-cover'
+            width={50}
+            height={50}
+            alt={data.username}
+            src={data.image}></Image>
         </div>
-      </main>
+        <main className='relative flex-grow pt-1 pl-2'>
+          <header className=''>
+            <span className='flex gap-1 font-mukta text-xl leading-none tracking-wider text-themePrimary-50'>
+              {data.name}
+              {data.authorVerified && (
+                <span className='group/verified relative'>
+                  <MdVerified></MdVerified>
+                  <span className='absolute left-1/2 top-6 hidden w-max -translate-x-1/2 rounded-md border border-none bg-black/90 px-2 py-1 font-mukta text-xs font-thin tracking-wide text-themePrimary-50/90 group-hover/verified:block'>
+                    This user is verified by the plex team.
+                  </span>
+                </span>
+              )}
+            </span>
+            <span className='font-ibmplex text-sm leading-none tracking-wide text-themePrimary-50/60'>
+              @{data.username}
+            </span>
+          </header>
+
+          <div className='mt-3'>
+            <p className='font-mukta font-thin leading-tight tracking-wide text-themePrimary-50/95'>
+              <ProfileViewBio
+                preserveWhitespace={false}
+                bioText={data.bio}></ProfileViewBio>
+            </p>
+          </div>
+        </main>
+      </Link>
     </article>
   );
 };
