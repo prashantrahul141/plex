9a25cf42cbde2f24b3b60767d5ac08fb9d9ad646
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-19 23:04:15 +0530

    adding file

---
+++ src/components/views/commentView.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+import type { IReturnComment } from 'src/types';
+
+const CommentView: FC<{ data: IReturnComment }> = ({ data }) => {
+  return <></>;
+};
+export default CommentView;
