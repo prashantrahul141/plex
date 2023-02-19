5b334e3a24461b1ff563347f28e01f05d5cb8d2a
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-20 00:10:40 +0530

    using endpoint to create comment

--- src/components/forms/commentform.tsx
+++ src/components/forms/commentform.tsx
@@ -1,31 +1,58 @@
+import ErrorMessage from '@components/common/errorMessage';
+import { api } from '@utils/api';
+import Image from 'next/image';
 import type { FC } from 'react';
 import { useForm } from 'react-hook-form';
-import { PostComment } from 'src/types';
+import type { IPostComment } from 'src/types';
 
 const CommentForm: FC<{ postId: string; authorImage: string }> = ({
   authorImage,
   postId,
 }) => {
   const {
-    watch,
     register,
     handleSubmit,
+    resetField,
     formState: { errors },
-  } = useForm<PostComment>({ mode: 'all' });
-
-  const postComment = () => {
-    console.log();
+  } = useForm<IPostComment>({ mode: 'all' });
+  const createCommentQuery = api.comments.create.useMutation();
+  const postComment = async (data: IPostComment) => {
+    resetField('commentText');
+    await createCommentQuery.mutateAsync({
+      commentText: data.commentText,
+      postId,
+    });
   };
 
   return (
-    <form className='w-full'>
-      <input
-        title='Comment'
-        placeholder='Your comment'
-        type='text'
-        {...register('commentText', {
-          maxLength: { value: 200, message: 'Too long for a comment.' },
-        })}></input>
+    <form className='w-full px-1 py-2' onSubmit={handleSubmit(postComment)}>
+      <div className='flex w-full items-center justify-center gap-2'>
+        <Image
+          alt='Avatar'
+          src={authorImage}
+          width={50}
+          height={50}
+          className='h-12 min-h-[3rem] w-12 min-w-[3rem] rounded-full'></Image>
+
+        <textarea
+          title='Comment'
+          className='textarea h-12 flex-grow'
+          placeholder='Your comment'
+          {...register('commentText', {
+            maxLength: { value: 200, message: 'Too long for a comment.' },
+          })}></textarea>
+
+        <button
+          type='submit'
+          className='btn h-fit w-fit rounded-3xl bg-themePrimary-400/90 px-2 text-themePrimary-50/95 hover:bg-themePrimary-400'>
+          Comment
+        </button>
+      </div>
+      <div className='pl-14'>
+        {errors.commentText && (
+          <ErrorMessage message={errors.commentText.message}></ErrorMessage>
+        )}
+      </div>
     </form>
   );
 };
