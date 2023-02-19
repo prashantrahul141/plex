9a434fb92385e886205e55d1653b844a0f66e9c0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-19 21:17:49 +0530

    adding  getAuthorFollowers, getAuthorFollowings, getFollowersFromUsername, getFollowingsFromUsername

--- src/server/api/routers/follows.ts
+++ src/server/api/routers/follows.ts
@@ -1,14 +1,113 @@
 import { z } from 'zod';
-import { createTRPCRouter, publicProcedure } from '../trpc';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { prisma } from 'src/server/db';
 
 export const FollowsRouter = createTRPCRouter({
-  ping: publicProcedure
-    .input(
-      z.object({
-        data: z.string().nullable(),
-      })
-    )
-    .query(({ input }) => {
-      return input.data;
+  // get current author's followers
+  getAuthorFollowers: protectedProcedure.query(async ({ ctx }) => {
+    const follows = await prisma.follows.findMany({
+      where: {
+        followingId: ctx.session.user.id,
+      },
+      select: {
+        follower: {
+          select: {
+            name: true,
+            username: true,
+            bio: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    return {
+      follows: follows.map((eachFollow) => {
+        return eachFollow.follower;
+      }),
+    };
+  }),
+
+  // get current author's followings
+  getAuthorFollowings: protectedProcedure.query(async ({ ctx }) => {
+    const follows = await prisma.follows.findMany({
+      where: {
+        followerId: ctx.session.user.id,
+      },
+      select: {
+        following: {
+          select: {
+            name: true,
+            username: true,
+            bio: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    return {
+      follows: follows.map((eachFollow) => {
+        return eachFollow.following;
+      }),
+    };
+  }),
+
+  // get followers from username
+  getFollowersFromUsername: protectedProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const follows = await prisma.follows.findMany({
+        where: {
+          following: {
+            username: input.username,
+          },
+        },
+        select: {
+          follower: {
+            select: {
+              name: true,
+              username: true,
+              bio: true,
+              image: true,
+            },
+          },
+        },
+      });
+
+      return {
+        follows: follows.map((eachFollow) => {
+          return eachFollow.follower;
+        }),
+      };
+    }),
+
+  // get follwings from username
+  getFollowingsFromUsername: protectedProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const follows = await prisma.follows.findMany({
+        where: {
+          follower: {
+            username: input.username,
+          },
+        },
+        select: {
+          follower: {
+            select: {
+              name: true,
+              username: true,
+              bio: true,
+              image: true,
+            },
+          },
+        },
+      });
+
+      return {
+        follows: follows.map((eachFollow) => {
+          return eachFollow.follower;
+        }),
+      };
     }),
 });
