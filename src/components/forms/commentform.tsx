4077b4df3cfb80caa803d49f50cd39ffd581ee2d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-19 23:04:09 +0530

    adding basic component

--- src/components/forms/commentform.tsx
+++ src/components/forms/commentform.tsx
@@ -1,6 +1,32 @@
 import type { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { PostComment } from 'src/types';
 
-const CommentForm: FC = () => {
-  return <form></form>;
+const CommentForm: FC<{ postId: string; authorImage: string }> = ({
+  authorImage,
+  postId,
+}) => {
+  const {
+    watch,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostComment>({ mode: 'all' });
+
+  const postComment = () => {
+    console.log();
+  };
+
+  return (
+    <form className='w-full'>
+      <input
+        title='Comment'
+        placeholder='Your comment'
+        type='text'
+        {...register('commentText', {
+          maxLength: { value: 200, message: 'Too long for a comment.' },
+        })}></input>
+    </form>
+  );
 };
 export default CommentForm;
