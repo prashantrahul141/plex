c2522498e6fba2d39c4469abcf72b6bf3e060a1d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-20 21:56:11 +0530

    getUnseen, getUnseenNumber, markSeen end points

--- src/server/api/routers/notification.ts
+++ src/server/api/routers/notification.ts
@@ -12,6 +12,38 @@ export const NotificationRouter = createTRPCRouter({
     return notifications;
   }),
 
+  // gets all unseen notificaions
+  getUnseen: protectedProcedure.query(async ({ ctx }) => {
+    const notifications = await prisma.notification.findMany({
+      where: { userId: ctx.session.user.id, seen: false },
+    });
+
+    return notifications;
+  }),
+
+  // gets number of all unseen notificaions
+  getUnseenNumber: protectedProcedure.query(async ({ ctx }) => {
+    const notifications = await prisma.notification.findMany({
+      where: { userId: ctx.session.user.id, seen: false },
+    });
+
+    return notifications.length;
+  }),
+
+  // mark a notificaion as seen
+  markSeen: protectedProcedure
+    .input(z.object({ notificationId: z.string() }))
+    .mutation(async ({ input }) => {
+      await prisma.notification.update({
+        data: {
+          seen: true,
+        },
+        where: {
+          id: input.notificationId,
+        },
+      });
+    }),
+
   // create a new notification
   create: protectedProcedure
     .input(
