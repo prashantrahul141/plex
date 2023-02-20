d021576ba6cdb0fb86f852f2a16cc84eaafefff3
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-20 22:06:48 +0530

    removing accidental rendering of number of unseen notifications

--- src/components/common/sidebarnavigation.tsx
+++ src/components/common/sidebarnavigation.tsx
@@ -75,7 +75,7 @@ const SideBarNavigation: FC<{
                   {eachTab.name}
                 </span>
                 {eachTab.name === 'Notifications' &&
-                  unseenNotifications.data &&
+                  unseenNotifications.data !== undefined &&
                   unseenNotifications.data > 0 && (
                     <span className='absolute top-0 right-0 z-10 flex h-fit min-h-[1.1rem] w-fit min-w-[1.1rem] items-center justify-center rounded-full bg-themePrimary-400 font-ibmplex text-[.5rem] not-italic leading-none text-themePrimary-50'>
                       {unseenNotifications.data}
