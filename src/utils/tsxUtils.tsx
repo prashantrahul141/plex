7808c4d31b500079c750d948f68a842fa2015e3d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-22 21:57:26 +0530

    adding replace functions separate utils file

---
+++ src/utils/tsxUtils.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link';
+import React from 'react';
+import reactStringReplace from 'react-string-replace';
+import {
+  HASHTAG_REGEX_EXP,
+  URL_REGEX_EXP,
+  USERNAME_REGEX_EXP,
+} from 'src/constantValues';
+
+export const hashtagReplace = (
+  text: string | React.ReactNode[] | undefined
+) => {
+  return reactStringReplace(text, HASHTAG_REGEX_EXP, (match, i) => {
+    return (
+      <Link
+        key={match + i.toString()}
+        title={match}
+        className='font-ibmplex leading-tight tracking-tighter text-themePrimary-300 hover:underline'
+        href={`/trending?q=${match.substring(1, match.length)}`}>
+        #{match}
+      </Link>
+    );
+  });
+};
+
+export const usernameReplace = (
+  text: string | React.ReactNode[] | undefined
+) => {
+  return reactStringReplace(text, USERNAME_REGEX_EXP, (match, i) => {
+    return (
+      <Link
+        title={match}
+        key={match + i.toString()}
+        className='font-ibmplex leading-tight tracking-tighter text-themePrimary-300 hover:underline'
+        href={`/${match.substring(1, match.length)}`}>
+        @{match}
+      </Link>
+    );
+  });
+};
+
+export const urlReplace = (text: string | React.ReactNode[] | undefined) => {
+  return reactStringReplace(text, URL_REGEX_EXP, (match, i) => {
+    try {
+      const url = new URL(match);
+      return (
+        <Link
+          title={match}
+          key={match + i.toString()}
+          className='font-ibmplex leading-tight tracking-tighter text-themePrimary-300 hover:underline'
+          href={url}>
+          {url.hostname}
+        </Link>
+      );
+    } catch {
+      return match;
+    }
+  });
+};
