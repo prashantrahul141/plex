d817f8cd776e3adadedf00f66413282ec579218c
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-22 21:41:16 +0530

    regex exp constants

--- src/constantValues.ts
+++ src/constantValues.ts
@@ -1,3 +1,4 @@
-const POSTS_PER_PAGE = 20;
-
-export default POSTS_PER_PAGE;
+export const POSTS_PER_PAGE = 20;
+export const USERNAME_REGEX_EXP = /@([A-Z_0-9]{1,16})/gi;
+export const MENTION_REGEX_EXP = /#([A-Z_0-9]+)/gi;
+export const URL_REGEX_EXP = /(https?:\/\/\S+)/gi;
