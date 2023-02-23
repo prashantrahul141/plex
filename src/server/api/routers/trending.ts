7edb15c97fe5b5a64ebd308884197f0522660c84
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-23 19:06:24 +0530

    adding getSpecificFromQuery with impl and getCurrentTrending to avoid errors in posts list component

--- src/server/api/routers/trending.ts
+++ src/server/api/routers/trending.ts
@@ -1,14 +1,95 @@
 import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { prisma } from 'src/server/db';
+import { POSTS_PER_PAGE } from 'src/constantValues';
 
 export const TrendingRouter = createTRPCRouter({
-  ping: protectedProcedure
-    .input(
-      z.object({
-        data: z.string().nullable(),
-      })
-    )
-    .query(({ input }) => {
-      return input.data;
+  // gets current trending page
+  getCurrentTrending: protectedProcedure
+    .input(z.object({ skip: z.number().default(0) }))
+    .query(async ({ input, ctx }) => {
+      const posts = await prisma.post.findMany({
+        take: POSTS_PER_PAGE,
+        skip: input.skip,
+        orderBy: { createdOn: 'asc' },
+        include: {
+          BookmarkedByAuthor: {
+            where: {
+              userId: ctx.session.user.id,
+            },
+          },
+          LikedByAuthor: {
+            where: {
+              userId: ctx.session.user.id,
+            },
+          },
+          _count: {
+            select: {
+              Comments: true,
+              LikedByAuthor: true,
+            },
+          },
+          Author: {
+            select: {
+              id: true,
+              image: true,
+              name: true,
+              username: true,
+              authorVerified: true,
+            },
+          },
+        },
+      });
+      return { posts };
+    }),
+
+  // gets posts with a specific hashtag
+  getSpecificFromQuery: protectedProcedure
+    .input(z.object({ query: z.string(), skip: z.number().default(0) }))
+    .query(async ({ input, ctx }) => {
+      const posts = await prisma.post.findMany({
+        take: POSTS_PER_PAGE,
+        skip: input.skip,
+        include: {
+          BookmarkedByAuthor: {
+            where: {
+              userId: ctx.session.user.id,
+            },
+          },
+          LikedByAuthor: {
+            where: {
+              userId: ctx.session.user.id,
+            },
+          },
+          _count: {
+            select: {
+              Comments: true,
+              LikedByAuthor: true,
+            },
+          },
+          Author: {
+            select: {
+              id: true,
+              image: true,
+              name: true,
+              username: true,
+              authorVerified: true,
+            },
+          },
+        },
+        where: {
+          HashtagOnPost: {
+            some: {
+              Hashtag: {
+                text: {
+                  equals: input.query,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return { posts };
     }),
 });
