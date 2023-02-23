3f6aa628244a8a32644dea623658ae8162ab8a72
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-23 19:05:22 +0530

    adding trending list component

--- src/components/lists/trendingList.tsx
+++ src/components/lists/trendingList.tsx
@@ -1,7 +1,30 @@
 import type { FC } from 'react';
+import { useRouter } from 'next/router';
+import type { Session } from 'next-auth';
+import PostList from './postsList';
 
-const TrendingList: FC = () => {
-  return <></>;
+const TrendingList: FC<{ session: Session }> = ({ session }) => {
+  const router = useRouter();
+  const { q } = router.query;
+
+  // sets idToFind to username or id of current user.
+  let queryToFind: null | string = null;
+
+  // if viewing other's profile
+  if (typeof q === 'string') {
+    queryToFind = q;
+  } else if (Array.isArray(q) && q[0] !== undefined) {
+    queryToFind = q[0];
+  }
+
+  return (
+    <>
+      <PostList
+        authorId={session.user.id}
+        trendingList={true}
+        trendingQuery={queryToFind}></PostList>
+    </>
+  );
 };
 
 export default TrendingList;
