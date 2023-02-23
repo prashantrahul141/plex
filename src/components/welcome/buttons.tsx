97a4ec859013496a9d419fca80060ec582dff396
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-23 22:11:43 +0530

    using links instead of routers to redirect from welcome page

--- src/components/welcome/buttons.tsx
+++ src/components/welcome/buttons.tsx
@@ -1,19 +1,19 @@
-import type { NextRouter } from 'next/router';
 import type { FC } from 'react';
+import Link from 'next/link';
 
-const WelcomePageButton: FC<{ router: NextRouter }> = ({ router }) => {
+const WelcomePageButton: FC = () => {
   return (
     <div className='mx-auto mt-60 flex w-fit gap-4'>
-      <button
-        onClick={() => void router.push('/signin')}
-        className='btn hover:bg-themePrimary-30080 w-28 rounded-lg bg-themePrimary-300 hover:bg-gradient-to-bl hover:from-themePrimary-50/20 hover:to-transparent hover:text-themePrimary-50 '>
-        Sign in
-      </button>
-      <button
-        onClick={() => void router.push('/home')}
-        className='btn w-28 rounded-md text-themePrimary-50/70 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
-        Explore
-      </button>
+      <Link href='/signin' aria-label='Sign in page'>
+        <button className='btn hover:bg-themePrimary-30080 w-28 rounded-lg bg-themePrimary-300 hover:bg-gradient-to-bl hover:from-themePrimary-50/20 hover:to-transparent hover:text-themePrimary-50 '>
+          Sign in
+        </button>
+      </Link>
+      <Link href='/home' aria-label='home page'>
+        <button className='btn w-28 rounded-md text-themePrimary-50/70 hover:border-themePrimary-300/60 hover:bg-transparent hover:text-themePrimary-50'>
+          Explore
+        </button>
+      </Link>
     </div>
   );
 };
