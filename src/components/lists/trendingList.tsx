5b708f6b72cff734a131d16108348b3bc132226f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-28 23:24:52 +0530

    showing trending hashtag list when query is not present

--- src/components/lists/trendingList.tsx
+++ src/components/lists/trendingList.tsx
@@ -1,28 +1,56 @@
 import type { FC } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import type { Session } from 'next-auth';
 import PostList from './postsList';
+import { api } from '@utils/api';
+import TrendingItemView from '@components/views/trendingItemView';
+import type { TReturnHashtag } from 'src/types';
 
 const TrendingList: FC<{ session: Session }> = ({ session }) => {
   const router = useRouter();
   const { q } = router.query;
+  const [trendingListState, setTrendingListState] = useState<
+    Array<TReturnHashtag>
+  >([]);
 
-  // sets idToFind to username or id of current user.
   let queryToFind: null | string = null;
 
-  // if viewing other's profile
+  // if viewing specific hastag
   if (typeof q === 'string') {
     queryToFind = q;
   } else if (Array.isArray(q) && q[0] !== undefined) {
     queryToFind = q[0];
   }
 
+  const trendingListQuery = api.trending.getCurrentTrending.useQuery(
+    { take: 10 },
+    {
+      enabled: !queryToFind,
+    }
+  );
+
+  useEffect(() => {
+    if (trendingListQuery.data) {
+      setTrendingListState(trendingListQuery.data);
+    }
+  }, [trendingListQuery.data]);
+
   return (
     <>
-      <PostList
-        authorId={session.user.id}
-        trendingList={true}
-        trendingQuery={queryToFind}></PostList>
+      {queryToFind && (
+        <PostList
+          authorId={session.user.id}
+          trendingList={true}
+          trendingQuery={queryToFind}></PostList>
+      )}
+      {!queryToFind &&
+        trendingListState.map((eachTrending) => (
+          <TrendingItemView
+            key={eachTrending.id}
+            hashtagData={eachTrending}></TrendingItemView>
+        ))}
     </>
   );
 };
