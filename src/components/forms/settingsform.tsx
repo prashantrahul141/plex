81871da8e4e4c00dcf5daa89947487cd3d748079
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-01 00:16:49 +0530

    styling for settings form

--- src/components/forms/settingsform.tsx
+++ src/components/forms/settingsform.tsx
@@ -6,18 +6,20 @@ import Link from 'next/link';
 const SettingsForm: FC = () => {
   return (
     <main className='h-full w-full'>
-      <section className='flex w-full items-center border-y border-y-themePrimary-100/40  py-2 pl-4 font-mukta text-lg tracking-wide text-themePrimary-50/95'>
-        <Link href={'/profile/edit'}>
+      <Link
+        href={'/profile/edit'}
+        className='flex w-full items-center border-y border-y-themePrimary-100/40 py-2  pl-4 font-mukta text-lg tracking-wide text-themePrimary-50/95 hover:bg-themePrimary-50/5'>
+        <span>
           Edit Profile <span className='text-themePrimary-50/40'>{'>'}</span>
-        </Link>
-      </section>
+        </span>
+      </Link>
 
       <div className='mt-4 flex w-full items-center justify-center'>
         <button
           onClick={() => {
             void signOut();
           }}
-          className='btn w-fit rounded-xl border-red-500 px-3 py-[.3rem] text-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white'>
+          className='btn w-fit rounded-xl border-red-500 px-3 py-[.3rem] text-red-500 hover:border-red-500/95 hover:bg-red-500/95 hover:text-white'>
           <span className='flex items-center justify-center gap-2'>
             <FaSignOutAlt></FaSignOutAlt>Sign Out
           </span>
