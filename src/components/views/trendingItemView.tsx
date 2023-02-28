519ce48141541fe025088e445d95fa1d526763bb
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-28 23:24:13 +0530

    using TReturnHashtag

--- src/components/views/trendingItemView.tsx
+++ src/components/views/trendingItemView.tsx
@@ -1,25 +1,24 @@
 import Link from 'next/link';
 import type { FC } from 'react';
+import type { TReturnHashtag } from 'src/types';
 
-const TrendingSideView: FC<{
-  hashTag: string;
-  postsNumber: number;
-  subText?: string;
-}> = ({ hashTag, postsNumber, subText }) => {
+const TrendintItemView: FC<{
+  hashtagData: TReturnHashtag;
+}> = ({ hashtagData }) => {
   return (
     <article className='rounded-xs group bg-baseBackground-100/80 p-2 hover:bg-baseBackground-100'>
-      <Link className='' href={`/trending?query=${hashTag}`}>
+      <Link className='' href={`/trending?q=${hashtagData.text}`}>
         <span className=' font-ibmplex text-xs text-themePrimary-50/40'>
-          {postsNumber} Posts
+          {hashtagData._count.HashtagOnPost} Posts
         </span>
 
         <h2 className='my-[1px] font-mukta text-xl tracking-wider text-themePrimary-400 group-hover:text-themePrimary-300'>
-          #{hashTag}
+          #{hashtagData.text}
         </h2>
 
-        {subText && (
+        {hashtagData.subtext && (
           <h5 className='font-mukta text-sm font-thin leading-tight tracking-wide text-themePrimary-50/40'>
-            {subText}
+            {hashtagData.subtext}
           </h5>
         )}
       </Link>
@@ -27,4 +26,4 @@ const TrendingSideView: FC<{
   );
 };
 
-export default TrendingSideView;
+export default TrendintItemView;
