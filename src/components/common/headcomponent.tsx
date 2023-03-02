6e50a63f480675e4a84b33d8a04cc8a9ea15ecfb
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-03 00:26:30 +0530

    adding more meta tags

--- src/components/common/headcomponent.tsx
+++ src/components/common/headcomponent.tsx
@@ -12,19 +12,30 @@ const HeadComp: FC<{ headTitle?: string }> = ({ headTitle = undefined }) => {
           <title>{`Plex | ${headTitle}`}</title>
         )}
 
+        <link rel='manifest' href='/manifest.json'></link>
+        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png'></link>
+
+        <meta name='darkreader-lock' />
+
+        <meta name='title' charSet='UTF-8' content='Plex' />
         <meta
           name='description'
           charSet='UTF-8'
-          content='A Social Media Application.'
+          content='Get together with Plex.'
         />
-        <link rel='manifest' href='/manifest.json'></link>
-        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png'></link>
-        <meta name='darkreader-lock' />
+
         <meta content='Plex' property='og:title' />
-        <meta content='Get together.' property='og:description' />
-        <meta content='http://localhost:3000' property='og:url' />
-        <meta content='' property='og:image' />
+        <meta content='Get together with Plex.' property='og:description' />
+        <meta content='https://plex-social.vercel.app/' property='og:url' />
+        <meta content='/static/cardimage.png' property='og:image' />
         <meta content='#141d31' data-react-helmet='true' name='theme-color' />
+
+        <meta name='twitter:card' content='/static/cardimage.png' />
+        <meta property='twitter:domain' content='plex-social.vercel.app' />
+        <meta property='twitter:url' content='https://plex-social.vercel.app' />
+        <meta name='twitter:title' content='Plex' />
+        <meta name='twitter:description' content='Get together with Plex.' />
+        <meta name='twitter:image' content='/static/cardimage.png' />
       </Head>
     </>
   );
