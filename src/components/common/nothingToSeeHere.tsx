9e48d42ae6b51f9859227d887a3dc95b7dfd4fdd
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-02 21:38:53 +0530

    adding component

--- src/components/common/nothingToSeeHere.tsx
+++ src/components/common/nothingToSeeHere.tsx
@@ -1,9 +1,17 @@
 import type { FC } from 'react';
+import { RiZzzFill } from 'react-icons/ri';
 
-const NothingToSeeHere: FC<{ text: string }> = ({
-  text = 'Nothing to see here.',
+const NothingToSeeHere: FC<{ text?: JSX.Element }> = ({
+  text = <span>Nothing to see here...</span>,
 }) => {
-  return <>{text}</>;
+  return (
+    <main className='mt-12 w-full'>
+      <section className='mx-auto flex w-fit max-w-lg flex-col items-center justify-center gap-2 text-center'>
+        <RiZzzFill size={50} className='text-themePrimary-50'></RiZzzFill>
+        <span className='font-mukta text-lg text-themePrimary-50'>{text}</span>
+      </section>
+    </main>
+  );
 };
 
 export default NothingToSeeHere;
