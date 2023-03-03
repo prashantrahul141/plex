cc6226255eadf273287baaa1b7b1bdd3af98d179
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-03 22:37:04 +0530

    adding footer widget

---
+++ src/components/additionalWidgetComponents/footerWidget.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import type { FC } from 'react';
+const FooterWidget: FC = () => {
+  return (
+    <div className='ml-4 mt-6'>
+      <span className='font-mukta text-xl leading-none tracking-wide text-themePrimary-50'>
+        Links
+      </span>
+      <section className='ml-2 mt-2 font-mukta font-thin tracking-wide text-themePrimary-50/40'>
+        <main className=' flex flex-wrap gap-2'>
+          <Link href={'/about'} className='hover:underline' title='About Page'>
+            About
+          </Link>
+          <Link
+            href={'https://github.com/prashantrahul141/plex'}
+            className='hover:underline'
+            passHref={true}
+            title='Source Code on Github'
+            target='_blank'>
+            Github
+          </Link>
+        </main>
+        <Link href={'/plex'} className=''>
+          <span className='font-ibmplex'>@</span>Plex&nbsp;
+          <span className='font-ibmplex tracking-tight'>2023</span>
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default FooterWidget;
