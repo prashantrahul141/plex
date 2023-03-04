9505fc3abd49b18111fbdd2b494b4de9087bdc94
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-04 12:36:57 +0530

    fix: blurry icon

--- src/components/common/navigationBar/navigationIcon.tsx
+++ src/components/common/navigationBar/navigationIcon.tsx
@@ -7,11 +7,11 @@ const NavigationIcon: FC = () => {
     <div className='mb-8 mt-2 flex select-none justify-center xl:justify-start'>
       <Link href={'/'} className='group flex items-center gap-1'>
         <Image
-          className=''
+          className='h-8 w-8'
           priority
           src='/favicon.ico'
-          width={32}
-          height={32}
+          width={64}
+          height={64}
           alt='Plex'></Image>
         <span className='hidden pt-1 font-mukta text-[1.6rem] leading-none tracking-wider text-themePrimary-50/20 xl:group-hover:block'>
           lex
