95f49ea57926111b38bbdefe62f9056b325d60cd
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-05 21:31:01 +0530

    refactoring, adding emoji picker.

--- src/components/forms/createpost/createpostform.tsx
+++ src/components/forms/createpost/createpostform.tsx
@@ -3,9 +3,8 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
-import { BsImage } from 'react-icons/bs';
+import { BsFillEmojiSmileFill, BsImage } from 'react-icons/bs';
 import { MdDeleteForever } from 'react-icons/md';
-import { AnimatePresence, motion } from 'framer-motion';
 import { env } from 'src/env/client.mjs';
 import { api } from '@utils/api';
 import type { AxiosProgressEvent } from 'axios';
@@ -15,6 +14,7 @@ import { useRouter } from 'next/router';
 import ErrorMessage from '@components/common/errorMessage';
 import type { IFormInput } from 'src/types';
 import { AiFillCloseCircle } from 'react-icons/ai';
+import EmojiPicker, { Theme } from 'emoji-picker-react';
 
 const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
   formSetCallback,
@@ -25,6 +25,8 @@ const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
   const {
     register,
     setFocus,
+    setValue,
+    getValues,
     formState: { errors },
     handleSubmit,
   } = useForm<IFormInput>({ mode: 'all' });
@@ -34,6 +36,7 @@ const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
   }, [setFocus]);
 
   const [postImageState, setPostImageState] = useState<File | null>(null);
+  const [showEmojiMenu, setShowEmojiMenu] = useState(false);
   const [postImageObjectUrlState, setPostImageObjectUrlState] = useState<
     string | null
   >(null);
@@ -50,7 +53,7 @@ const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
       postText: formData.postText,
       postImages: formData.postImages,
     });
-    formSetCallback(false);
+    formCallback();
     void router.push(
       `/${mutatedPost.createdPost.Author.username}/${mutatedPost.createdPost.id}`
     );
@@ -100,59 +103,59 @@ const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
     }
   };
 
+  const formCallback = () => {
+    formSetCallback(false);
+    setShowEmojiMenu(false);
+  };
+
   return (
-    <form
-      className='max-w-screen relative w-96 rounded-lg border border-themePrimary-100/10 bg-baseBackground-100 px-2 pb-2'
-      onSubmit={handleSubmit(submitForm)}>
-      {uploadingImageProgress !== null && (
-        <>
-          <div className='absolute top-0 left-0 z-30 h-full w-full max-w-md rounded backdrop-brightness-50'>
-            <div className='absolute top-1/2 left-1/2 h-10 w-10 -translate-y-1/2 -translate-x-1/2 text-center'>
-              <LoadingComponent></LoadingComponent>
-              <p className='mt-1 w-fit rounded-md bg-black/50 px-2 py-1 font-ibmplex text-sm tracking-wider  text-themePrimary-50'>
-                {parseInt((uploadingImageProgress * 100).toString())}%
-              </p>
+    <div>
+      <form
+        className='max-w-screen relative max-h-screen w-96 rounded-lg border border-themePrimary-100/10 bg-baseBackground-100 px-2 pb-2'
+        onSubmit={handleSubmit(submitForm)}>
+        {uploadingImageProgress !== null && (
+          <>
+            <div className='absolute top-0 left-0 z-30 h-full w-full max-w-md rounded backdrop-brightness-50'>
+              <div className='absolute top-1/2 left-1/2 h-10 w-10 -translate-y-1/2 -translate-x-1/2 text-center'>
+                <LoadingComponent></LoadingComponent>
+                <p className='mt-1 w-fit rounded-md bg-black/50 px-2 py-1 font-ibmplex text-sm tracking-wider  text-themePrimary-50'>
+                  {parseInt((uploadingImageProgress * 100).toString())}%
+                </p>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-      <div className='w-full'></div>
+          </>
+        )}
+        <div className='w-full'></div>
 
-      <div className='py-4 text-center'>
-        <span className='flex items-center justify-center font-mukta text-2xl leading-tight tracking-wide text-themePrimary-100/95'>
-          <span className='flex-grow'> Create a post</span>
-          <AiFillCloseCircle
-            title='close'
-            className='mr-4 mb-3 h-5 w-5 cursor-pointer text-base text-red-400 hover:text-red-500'
-            onClick={() => formSetCallback(false)}></AiFillCloseCircle>
-        </span>
-      </div>
-      <textarea
-        placeholder='Your thoughts here'
-        className='textarea mb-1'
-        {...register('postText', {
-          required: { value: true, message: 'Forgot to type something here?' },
-          maxLength: {
-            value: 150,
-            message: 'A post cannot be longer than 150 characters :/',
-          },
-        })}></textarea>
+        <div className='py-4 text-center'>
+          <span className='flex items-center justify-center font-mukta text-2xl leading-tight tracking-wide text-themePrimary-100/95'>
+            <span className='flex-grow'> Create a post</span>
+            <AiFillCloseCircle
+              title='close'
+              className='mr-4 mb-3 h-5 w-5 cursor-pointer text-base text-red-400 hover:text-red-500'
+              onClick={() => formCallback()}></AiFillCloseCircle>
+          </span>
+        </div>
+        <textarea
+          placeholder='Your thoughts here'
+          className='textarea mb-1'
+          {...register('postText', {
+            required: {
+              value: true,
+              message: 'Forgot to type something here?',
+            },
+            maxLength: {
+              value: 150,
+              message: 'A post cannot be longer than 150 characters :/',
+            },
+          })}></textarea>
 
-      {errors.postText && (
-        <ErrorMessage message={errors.postText.message}></ErrorMessage>
-      )}
+        {errors.postText && (
+          <ErrorMessage message={errors.postText.message}></ErrorMessage>
+        )}
 
-      <AnimatePresence>
         {postImageObjectUrlState !== null && (
-          <motion.div
-            className='my-8 w-full '
-            initial={{ scale: 0, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0, opacity: 0 }}
-            transition={{
-              ease: 'easeInOut',
-              duration: 0.3,
-            }}>
+          <div className='my-8 w-full '>
             <div className='relative mx-auto w-36'>
               <img alt='Image' className='' src={postImageObjectUrlState}></img>
               <button
@@ -173,47 +176,63 @@ const CreatePostForm: FC<{ formSetCallback: (value: boolean) => void }> = ({
                   size={24}></MdDeleteForever>
               </button>
             </div>
-          </motion.div>
+          </div>
         )}
-      </AnimatePresence>
-      <AnimatePresence>
-        {postImageObjectUrlState === null && (
-          <motion.div
-            className='my-4 ml-1 w-min'
-            initial={{ scale: 0, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0, opacity: 0 }}
-            transition={{
-              ease: 'easeInOut',
-              duration: 0.3,
-            }}>
-            <label htmlFor='contained-button-file' className='cursor-pointer'>
-              <input
-                accept={'.png, .jpg, .jpeg'}
-                className='hidden'
-                id='contained-button-file'
-                type='file'
-                onChange={({ target: { files } }) => {
-                  if (files !== null && files[0] !== undefined) {
-                    setPostImageState(files[0]);
-                    const objectUrl = URL.createObjectURL(files[0]);
-                    setPostImageObjectUrlState(objectUrl);
-                  }
-                }}
-              />
-              <BsImage
-                size={18}
-                className='text-themePrimary-300/70 hover:text-themePrimary-300/95'></BsImage>
-            </label>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <button
-        className='btn bg-themePrimary-400/90 text-themePrimary-50/95 hover:bg-themePrimary-400'
-        type='submit'>
-        Post
-      </button>
-    </form>
+
+        <div className='my-4 ml-1 flex w-max items-center gap-2'>
+          {postImageObjectUrlState === null && (
+            <div className='w-min'>
+              <label htmlFor='contained-button-file' className='cursor-pointer'>
+                <input
+                  accept={'.png, .jpg, .jpeg'}
+                  className='hidden'
+                  id='contained-button-file'
+                  type='file'
+                  onChange={({ target: { files } }) => {
+                    setShowEmojiMenu(false);
+                    if (files !== null && files[0] !== undefined) {
+                      setPostImageState(files[0]);
+                      const objectUrl = URL.createObjectURL(files[0]);
+                      setPostImageObjectUrlState(objectUrl);
+                    }
+                  }}
+                />
+                <BsImage
+                  size={18}
+                  title='Add image'
+                  className='text-themePrimary-300/70 hover:text-themePrimary-300/95'></BsImage>
+              </label>
+            </div>
+          )}
+          <BsFillEmojiSmileFill
+            onClick={() => setShowEmojiMenu(!showEmojiMenu)}
+            title={'Add emoji'}
+            className={`hidden cursor-pointer text-themePrimary-300/70 hover:text-themePrimary-300/95 sm:block ${
+              showEmojiMenu ? 'text-themePrimary-300/95' : ''
+            }`}></BsFillEmojiSmileFill>
+        </div>
+
+        <button
+          className='btn bg-themePrimary-400/90 text-themePrimary-50/95 hover:bg-themePrimary-400'
+          type='submit'>
+          Post
+        </button>
+      </form>
+      {showEmojiMenu && (
+        <div className='fixed top-1/2 left-1/3 z-50'>
+          <EmojiPicker
+            autoFocusSearch={true}
+            theme={Theme.DARK}
+            onEmojiClick={(e) => {
+              setValue('postText', getValues('postText') + e.emoji, {
+                shouldValidate: true,
+              });
+            }}
+            previewConfig={{ showPreview: false }}
+            height={400}></EmojiPicker>
+        </div>
+      )}
+    </div>
   );
 };
 
